c8f6104621bc6bba3fc660417d87600f25e00331
PLMCHL <pm.deblois@gmail.com>
2020-11-06 19:39:40 -0800

    Remove boon from the selected list

--- scripts.js
+++ scripts.js
@@ -24,17 +24,25 @@ $('.filter-god').click(function() {
   return false;
 });
 
-$('#all-boons-list .boon').click(function() {
+
+$('#all-boons-list').on('click', '.boon', function() {
 
   var boonName = $(this).find(".name").text();
 
   var selectedItem = selectedBoonsList.get('name', boonName);
 
-
   if (selectedItem.length == 0){
     var boonItem = boonsList.get('name', boonName);
     selectedBoonsList.add(boonItem[0].values());
   }
+
+});
+
+$('#selected-boons-list').on('click', '.boon', function() {
+
+  var boonName = $(this).find(".name").text();
+
+  var selectedItem = selectedBoonsList.remove('name', boonName);
 
 });
 
